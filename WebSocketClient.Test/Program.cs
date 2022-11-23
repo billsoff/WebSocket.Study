b4ce22c81a811dd53e1afb045273878d9e208d3f
billsoff <billsoff@hotmail.com>
2022-11-23 15:20:27 +0900

    ClientWebSocket test.

---
+++ WebSocketClient.Test/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.WebSockets;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace WebSocketClient.Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            WebSocket socket = CreateSocket().Result;
+
+            socket.SendMessageAsync("Sweet").Wait();
+
+            string echo = socket.ReadMessageAsync().Result;
+
+            Console.WriteLine(echo);
+
+            Console.WriteLine("Press any key to exit...");
+            Console.ReadKey(true);
+        }
+
+        private static async Task<ClientWebSocket> CreateSocket()
+        {
+            ClientWebSocket socket = new ClientWebSocket();
+
+            await socket.ConnectAsync(new Uri("ws://127.0.0.1:8089/"), CancellationToken.None);
+
+            return socket;
+        }
+    }
+}
